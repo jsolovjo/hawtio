e69f506acbb976335ec915e62d45ec98ea4c9a2f
Juri Solovjov <jsolovjo@redhat.com>
2025-04-30 07:51:39 +0200

    [E2E] Debug Camel Send 3

--- tests/hawtio-test-suite/src/test/java/io/hawt/tests/setup/suites/CucumberTest.java
+++ tests/hawtio-test-suite/src/test/java/io/hawt/tests/setup/suites/CucumberTest.java
@@ -1,5 +1,6 @@
 package io.hawt.tests.setup.suites;
 
+import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
@@ -7,6 +8,7 @@ import org.junit.platform.suite.api.Suite;
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("io/hawt/tests/features/")
+@ConfigurationParameter(key = "cucumber.publish.enabled", value = "false")
 public class CucumberTest {
 
 }
